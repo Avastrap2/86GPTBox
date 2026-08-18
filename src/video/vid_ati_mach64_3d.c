0e4e0d66d6c24a1f41b83dbde50461384f924d5a
Phil005 <110710503+Avastrap2@users.noreply.github.com>
2026-08-18 08:41:39 +0700

    video: assemble ATI 3D RAGE renderer

--- src/video/vid_ati_mach64_3d.c
+++ src/video/vid_ati_mach64_3d.c
@@ -1,1 +1,5 @@
-PLACEHOLDER
+/* ATI 3D RAGE / Rage II+ software renderer. */
+#include "vid_ati_mach64_3d_part1.inc"
+#include "vid_ati_mach64_3d_part2.inc"
+#include "vid_ati_mach64_3d_part3.inc"
+#include "vid_ati_mach64_3d_part4.inc"
