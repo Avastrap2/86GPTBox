1222e122fb9eed78161b3626285af2492acfa382
Phil005 <110710503+Avastrap2@users.noreply.github.com>
2026-08-24 20:42:02 +0700

    Use local DirectX 5 interface IIDs

--- tools/rage2p_win95_test/rage2p_test.c
+++ tools/rage2p_win95_test/rage2p_test.c
@@ -1,4 +1,6 @@
 #include "r2ptest_part_00.inc"
+#define IID_IDirect3D2 IID_R2P_IDirect3D2
+#define IID_IDirect3DTexture2 IID_R2P_IDirect3DTexture2
 #include "r2ptest_part_01.inc"
 #include "r2ptest_part_02.inc"
 #include "r2ptest_part_03.inc"
